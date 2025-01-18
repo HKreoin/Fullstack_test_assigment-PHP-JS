d6d6555dde39b805400429337f5a14e95f50a54d
Kreoin <steamuser85@yandex.ru>
2025-01-19 00:45:09 +0300

    meshap cars in init.sql

--- database/init_sqlite.sql
+++ database/init_sqlite.sql
@@ -24,11 +24,11 @@ CREATE TABLE cars (
 
 INSERT INTO cars (brand_id, model, start_date, end_date, body_type, image) VALUES
 (1, 'Camry', '2014-01-01', '2016-12-31', 'Sedan', 'camry.jpg'),
+(1, 'RAV4', '2018-01-01', NULL, 'SUV', 'rav4.jpg'),
+(3, 'Tiguan', '2012-01-01', '2017-05-10', 'SUV', 'tiguan.jpg'),
 (2, 'Fusion', '2017-01-01', NULL, 'Sedan', 'fusion.jpg'),
 (3, 'Jetta', '2016-01-01', '2019-12-31', 'Sedan', 'jetta.jpg'),
-(1, 'RAV4', '2018-01-01', NULL, 'SUV', 'rav4.jpg'),
-(2, 'Escape', '2014-01-01', '2018-09-10', 'SUV', 'escape.jpg'),
-(3, 'Tiguan', '2012-01-01', '2017-05-10', 'SUV', 'tiguan.jpg');
+(2, 'Escape', '2014-01-01', '2018-09-10', 'SUV', 'escape.jpg');
 
 CREATE TABLE car_repairs (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
