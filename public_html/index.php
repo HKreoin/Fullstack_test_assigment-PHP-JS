7028f95b83265ffd250d5209149d93322e08c668
Kreoin <steamuser85@yandex.ru>
2025-01-19 12:16:22 +0300

    sepoarate Database connection logic

--- public_html/index.php
+++ public_html/index.php
@@ -11,25 +11,9 @@ require_once __DIR__ . '/../database/config.php';
 
 use App\Router;
 
-$conn = null;
-
-$dsn = DRIVER === 'sqlite'
-    ? DRIVER . ':' . DB_NAME
-    : DRIVER . ':host=' . HOST . ';dbname=' . DB_NAME . ";charset=" . CHARSET;
-
-try {
-    $conn = new PDO($dsn, USER, PASS);
-} catch (PDOException $e) {
-    exit('Connection failed: ' . $e->getMessage());
-}
-
-$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$initFilePath = implode('/', [dirname(__DIR__), 'database/init_' . DRIVER . '.sql']);
-$initSql = file_get_contents($initFilePath);
-$conn->exec($initSql);
-
 require_once __DIR__ . '/../routes/web.php';
 
 $req = $_SERVER['REQUEST_METHOD'];
-$path = $_SERVER['REQUEST_URI'];
-echo Router::route($req, $path);
+$path = explode('?', $_SERVER['REQUEST_URI'])[0];
+$query = explode('?', $_SERVER['REQUEST_URI'])[1];
+Router::route($req, $path);
---
+++ src/Database.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use PDO;
+
+class Database
+{
+    private PDO $connection;
+
+    public function __construct($config)
+    {
+        $this->connect($config);
+    }
+
+    public function connect($config)
+    {
+        extract($config);
+        $dsn = $driver === 'sqlite'
+            ? $driver . ':' . $dbName
+            : $driver . ':host=' . $host . ';dbname=' . $dbName . ";charset=" . $charset;
+
+        try {
+            $conn = new PDO($dsn, $user, $password);
+        } catch (\PDOException $e) {
+            exit('Connection failed: ' . $e->getMessage());
+        }
+
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $initFilePath = implode('/', [dirname(__DIR__), 'database/init_' . $driver . '.sql']);
+        $initSql = file_get_contents($initFilePath);
+        $conn->exec($initSql);
+
+        $this->connection = $conn;
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
